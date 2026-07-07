a855cf9f8a7592abc05b051d94a6227dab5bf528
Yash Kumar Sharma <yashsharma4527@gmail.com>
2026-07-07 23:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (21.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> vec;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0);
+
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                if(matrix[i][j]==0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
 
         for(int i=0; i<matrix.size(); i++){
             for(int j=0; j<matrix[0].size(); j++){
-                if(matrix[i][j]==0){
-                    vec.push_back({i,j});
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
                 }
-            }
-        }
-
-        int n = vec.size();
-
-        for(int i=0; i<matrix.size(); i++){
-            int k = 0;
-            while(k<n){
-                if(vec[k].first == i){
-                    for(int j=0; j<matrix[0].size(); j++){
-                        matrix[i][j] = 0;
-                    }
-                }
-                k++;
-            }
-        }
-
-        for(int j=0; j<matrix[0].size(); j++){
-            int k = 0;
-            while(k<n){
-                if(vec[k].second == j){
-                    for(int i=0; i<matrix.size(); i++){
-                        matrix[i][j] = 0;
-                    }
-                }
-                k++;
             }
         }
     }
