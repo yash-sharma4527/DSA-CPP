ec8a8be6e710ecdf91a800cd93325ad52515619f
Yash Kumar Sharma <yashsharma4527@gmail.com>
2026-09-17 18:02:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (6.03%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_set<char> st;
+
+        for(char ch : s){
+            if(st.count(ch)){
+                return ch;
+            }
+
+            st.insert(ch);
+        }
+
+        return 'y';
+    }
+};
